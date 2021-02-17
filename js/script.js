9b853002e4a25cb6a5225f85ce3130e032cdd915
Alenka <ms.alenchik@gmail.com>
2021-02-17 23:23:38 +0200

    deleted function

--- js/script.js
+++ js/script.js
@@ -1,16 +1,7 @@
 const collButton = document.querySelectorAll(".faq__collapsible-button");
 const icon = document.querySelectorAll(".faq__collapsible-icon");
-const button = document.getElementById ("button")
-let i;
-const activeIcon = `<svg width="16" height="11" viewBox="0 0 16 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M1.88 10.5601L8 4.44006L14.12 10.5601L16 8.66673L8 0.666725L0 8.66673L1.88 10.5601Z" fill="#61A199"/>
-</svg>
-`
-const inactiveIcon = `<svg width="16" height="11" viewBox="0 0 16 11" fill="none" xmlns="http://www.w3.org/2000/svg">
-<path d="M1.88 0.439941L8 6.55994L14.12 0.439941L16 2.33327L8 10.3333L0 2.33327L1.88 0.439941Z" fill="#A0A7AD"/>
-</svg>
-`
 
+let i;
 
 for (i = 0; i < collButton.length; i++) {
     collButton[i].addEventListener("click", function () {
